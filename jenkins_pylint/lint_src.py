9e0d228e9502ca3166e165a0e8ad109a17320abc
marwan <marwan76@gmail.com>
2019-09-21 08:00:15 +0200

    changed lint_src.py

--- jenkins_pylint/lint_src.py
+++ jenkins_pylint/lint_src.py
@@ -5,15 +5,18 @@ import pylint.epylint
 
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
-REPO_ROOT = os.path.realpath(os.path.join(SCRIPT_DIR, "..", ".."))
+REPO_ROOT = os.path.realpath(os.path.join(SCRIPT_DIR))
 sys.path.insert(1, REPO_ROOT)
 
 PACKAGE = os.path.join(REPO_ROOT, "src")
+print(SCRIPT_DIR)
+print(REPO_ROOT)
+print(PACKAGE)
 
 #   config_file = os.path.realpath(os.path.join(REPO_ROOT, "cfg", "pylintrules.cfg"))
 """ Function where to find configuration file and the source files to run pylint on"""
 def run_lint():
-    config_file = os.path.realpath(os.path.join(REPO_ROOT, "\\Repos\\jenkins_pylint\\cfg", "default_pylintrules.cfg"))
+    config_file = os.path.realpath(os.path.join(REPO_ROOT, "\\cfg", "default_pylintrules.cfg"))
     return_code = pylint.epylint.lint(PACKAGE, ['--rcfile={}'.format(config_file)])
 #	if return_code:
 #		return False
